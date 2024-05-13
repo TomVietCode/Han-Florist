6895a2fa70128a65204b093366548a85f839a2cf
TomVietCode <mcuong04.work@gmail.com>
2024-05-13 11:04:46 +0700

    Nhúng file tĩnh

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express")
 const route = require("./routes/client/index.route.js")
 const dotenv = require("dotenv").config()
 
-
 const app = express()
 const port = process.env.PORT
 
+//Cấu hình pug
 app.set("views", "./views")
 app.set("view engine", "pug")
+
+//Nhúng file tĩnh
+app.use(express.static("public"))
 
 //Routes
 route(app)
