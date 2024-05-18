7595a9488737ce5f28847c5588347dd630094948
TomVietCode <mcuong04.work@gmail.com>
2024-05-18 10:44:47 +0700

    Tạo trang tổng quan

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const route = require("./routes/client/index.route.js")
 const dotenv = require("dotenv").config()
 const database = require("./config/database.js")
 
 database.connect()
+
+const routesAdmin = require("./routes/admin/index.route")
+const routesClient = require("./routes/client/index.route")
 
 const app = express()
 const port = process.env.PORT
@@ -15,8 +17,11 @@ app.set("view engine", "pug")
 //Nhúng file tĩnh
 app.use(express.static("public"))
 
-//Routes
-route(app)
+//Routes Admin
+routesAdmin(app)
+
+// Routes Client
+routesClient(app)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
