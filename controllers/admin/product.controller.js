ba39540e29e16e9888603a4d27a2b166bffee421
TomVietCode <mcuong04.work@gmail.com>
2024-05-18 21:54:11 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = ("/", (req, res) => {
+const Product = require("../../models/product.model")
+
+//[GET] /admin/product
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: "false"
+  })
+
   res.render("admin/pages/products/index.pug", {
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: products
   });
-});
+};
