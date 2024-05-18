f2164cae174f049bcb1dcb6a14f44d0aa652b2ea
TomVietCode <mcuong04.work@gmail.com>
2024-05-19 00:24:49 +0700

    Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -20,26 +20,34 @@ module.exports.index = async (req, res) => {
     }
   ]
 
-  if(req.query.status){
-    const index = filterStatus.findIndex(item => item.status == req.query.status)
-    filterStatus[index].class = "active"
-  }else{
-    filterStatus[0].class = "active"
-  }
-
   let find = {
     deleted: false
   }
 
   if(req.query.status){
     find.status = req.query.status;
+
+    const index = filterStatus.findIndex(item => item.status == req.query.status)
+    filterStatus[index].class = "active"
+  }else{
+    filterStatus[0].class = "active"
   }
-  
+
+  let keyword = ""
+
+  if(req.query.keyword){
+    keyword = req.query.keyword
+
+    const regex = new RegExp(keyword, "i")
+    find.title = regex
+  }
+
   const products = await Product.find(find)
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus,
+    keyword: keyword
   });
 };
