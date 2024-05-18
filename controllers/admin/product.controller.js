432790f69568880eca880298c1781677660ea9d9
TomVietCode <mcuong04.work@gmail.com>
2024-05-18 23:42:31 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,44 @@ const Product = require("../../models/product.model")
 
 //[GET] /admin/product
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    deleted: "false"
-  })
+  const filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: ""
+    },
+    {
+      name: "Đang hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    }
+  ]
+
+  if(req.query.status){
+    const index = filterStatus.findIndex(item => item.status == req.query.status)
+    filterStatus[index].class = "active"
+  }else{
+    filterStatus[0].class = "active"
+  }
+
+  let find = {
+    deleted: false
+  }
+
+  if(req.query.status){
+    find.status = req.query.status;
+  }
+  
+  const products = await Product.find(find)
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
-    products: products
+    products: products,
+    filterStatus: filterStatus
   });
 };
