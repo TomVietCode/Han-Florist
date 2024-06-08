7cb74727025d7ae0e55c9460eea82cdd415e9ba8
TomVietCode <mcuong04.work@gmail.com>
2024-06-08 07:13:32 +0700

    Hiển thị thông báo sau khi đổi trạng thái (Xử lý ẩn thông báo bên Frontend)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -179,3 +179,21 @@ if(formDeleteItem){
 // End Delete Item
 
 
+// Show Alert
+const showAlert = document.querySelector("[show-alert]")
+if(showAlert){
+  let dataTime = showAlert.getAttribute("data-time")
+  dataTime = parseInt(dataTime)
+
+  // Ẩn thông báo sau một khoảng thời gian
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden")
+  }, dataTime);
+
+  // Tắt thông báo khi nhấn nút "x"
+  const closeButton = showAlert.querySelector("[close-alert]")
+  closeButton.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden")
+  })
+}
+// End Show Alert
