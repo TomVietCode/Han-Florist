d11e14e84bac583ab012f7aeab5313ee4f90a2d1
TomVietCode <mcuong04.work@gmail.com>
2024-07-11 23:33:08 +0700

    Xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,5 +1,6 @@
 const Role = require("../../models/role.model")
 const systemConfig = require("../../config/system")
+
 // [GET] /admin/roles
 module.exports.index = async (req, res) => {
   const records = await Role.find({ deleted: false })
@@ -29,4 +30,34 @@ module.exports.createPost = async (req, res) => {
   }
 
   res.redirect(`/${systemConfig.prefixAdmin}/roles`)
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const data = await Role.findOne({ _id: id, deleted: false })
+
+    res.render("admin/pages/roles/edit.pug", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      data: data,
+    })
+  } catch (error) {
+    req.flash("error", "Không tìm thấy nhóm quyền hợp lệ!")
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`)
+  }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id
+
+  try {
+    await Role.updateOne({_id: id}, req.body)
+    req.flash("success", "Cập nhật thành công!")
+  } catch (error) {
+    req.flas("error", "Cập nhật thất bại")
+  }
+  res.redirect("back")
 }
