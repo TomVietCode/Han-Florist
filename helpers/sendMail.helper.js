f58f451a5ad0b036f1cdbf10d27d84e9f4fd98aa
TomVietCode <mcuong04.work@gmail.com>
2024-08-07 17:22:25 +0700

    .

--- helpers/sendMail.helper.js
+++ helpers/sendMail.helper.js
@@ -4,13 +4,13 @@ module.exports.sendEmail = (email, subject, html) => {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.MAIL_EMAIL,
+      user: "tiloi2k4@gmail.com",
       pass: "heyqhuxghigmcwcz"
     }
   });
   
   var mailOptions = {
-    from: process.env.MAIL_EMAIL,
+    from: "tiloi2k4@gmail.com",
     to: email,
     subject: subject,
     html: html
